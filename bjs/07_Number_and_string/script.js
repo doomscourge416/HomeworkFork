faa6188630d5f19f5a3edb935c5d39d1a56968d9
doomscourge416 <jet_7@list.ru>
2023-10-27 21:20:28 +0300

    modified example task

--- bjs/07_Number_and_string/script.js
+++ bjs/07_Number_and_string/script.js
@@ -1,12 +1,130 @@
 let lastOperand = 0;
 let operation = null;
 
-const inputWindow = document.getElementById('inputWindow');
+const inputWindow = document.querySelector('#inputWindow');
+
+// Кнопки цифр:
 
 
-document.getElementById('btn_clr').addEventListener('click', function () {
+document.querySelector('#btn_1').addEventListener('click', function () {
+    inputWindow.value += '1';
+})
+
+
+document.querySelector('#btn_2').addEventListener('click', function () {
+    inputWindow.value += '2';
+})
+
+
+document.querySelector('#btn_3').addEventListener('click', function () {
+    inputWindow.value += '3';
+})
+
+
+document.querySelector('#btn_4').addEventListener('click', function () {
+    inputWindow.value += '4';
+})
+
+
+document.querySelector('#btn_5').addEventListener('click', function () {
+    inputWindow.value += '5';
+})
+
+
+document.querySelector('#btn_6').addEventListener('click', function () {
+    inputWindow.value += '6';
+})
+
+
+document.querySelector('#btn_7').addEventListener('click', function () {
+    inputWindow.value += '7';
+})
+
+
+document.querySelector('#btn_8').addEventListener('click', function () {
+    inputWindow.value += '8';
+})
+
+
+document.querySelector('#btn_9').addEventListener('click', function () {
+    inputWindow.value += '9';
+})
+
+
+document.querySelector('#btn_0').addEventListener('click', function () {
+    inputWindow.value += '0';
+})
+
+
+
+// Кнопка "C":
+
+
+document.querySelector('#btn_clr').addEventListener('click', function () {
     lastOperand = 0;
     operation = null;
     inputWindow.value = '';
 })
 
+
+//Кнопки математических вычислений:
+
+
+document.querySelector('#btn_sum').addEventListener('click', function () {
+    lastOperand = parseInt(inputWindow.value);
+    operation = 'sum';
+    inputWindow.value = '';
+    
+})
+
+
+document.querySelector('#btn_subt').addEventListener('click', function () {
+    lastOperand = parseInt(inputWindow.value);
+    operation = 'subt';
+    inputWindow.value = '';
+    
+})
+
+
+document.querySelector('#btn_mult').addEventListener('click', function () {
+    lastOperand = parseInt(inputWindow.value);
+    operation = 'mult';
+    inputWindow.value = '';
+})
+
+
+document.querySelector('#btn_div').addEventListener('click', function () {
+    lastOperand = parseInt(inputWindow.value);
+    operation = 'div';
+    inputWindow.value = '';
+})
+
+
+// Кнопка "РАВНО":
+
+document.querySelector('#btn_eql').addEventListener('click', function () {
+    if (operation === 'sum'){
+        const result =  lastOperand + parseInt(inputWindow.value);
+        operation = null;
+        lastOperand = 0;
+        inputWindow.value = result;
+    }
+    if (operation === 'subt'){
+        const result = lastOperand - parseInt(inputWindow.value);
+        operation = null;
+        lastOperand = 0;
+        inputWindow.value = result;
+    }
+    if (operation === 'mult'){
+        const result = lastOperand * parseInt(inputWindow.value);
+        operation = null;
+        lastOperand = 0;
+        inputWindow.value = result;
+    }
+    if(operation === 'div'){
+        const result = lastOperand / parseInt(inputWindow.value);
+        operation = null
+        lastOperand = 0;
+        inputWindow.value = result;
+    }
+})
--- bjs/08_if_else/script.js
+++ bjs/08_if_else/script.js
@@ -1,29 +1,180 @@
+
+// let minValue
+// let maxValue
+// let otpavitCifri = document.querySelector('#minMaxSubmit')
+
+// // otpavitCifri.addEventListener('click',function(){
+    
+// //     gameRun = true
+// // } )
+
+
 let minValue = parseInt(prompt('Минимальное знание числа для игры','0'));
 let maxValue = parseInt(prompt('Максимальное знание числа для игры','100'));
+
+if(isNaN(minValue)) minValue = 0 ;
+if(isNaN(maxValue)) maxValue = 100 ;
+
+
+(minValue <= -1000) ? minValue = -999 : '';
+(maxValue >= 1000) ? maxValue = 999 : '';
+
+
+
 alert(`Загадайте любое целое число от ${minValue} до ${maxValue}, а я его угадаю`);
-let answerNumber  = Math.floor((minValue + maxValue) / 2);
+
+
+
+
+
+function numberToText(number) {
+    const decimals = ['ноль', 'один', 'два', 'три', 'четыре', 'пять', 'шесть', 'семь', 'восемь', 'девять', 'десять', 'одиннадцать', 'двенадцать', 'тринадцать', 'четырнадцать', 'пятнадцать', 'шестнадцать', 'семнадцать', 'восемнадцать', 'девятнадцать'];
+    const tens = ['', '', 'двадцать', 'тридцать', 'сорок', 'пятьдесят', 'шестьдесят', 'семьдесят', 'восемьдесят', 'девяносто'];
+    const hundreds = ['', 'сто', 'двести', 'триста', 'четыреста', 'пятьсот', 'шестьсот', 'семьсот', 'восемьсот', 'девятьсот'];
+
+    let result = ' ';
+
+    if(number < 0){
+        number = Math.abs(number);
+    
+        if(number < 20) {
+
+            result = decimals[number];
+    
+        } else if(number < 100) {
+    
+            result = tens[Math.floor(number / 10)];
+    
+            if(number % 10 !== 0) {
+    
+                result += ' ' + decimals[number % 10];
+            }
+        } else if (number < 1000) {
+    
+            result = hundreds [Math.floor(number / 100)];
+    
+            if (number % 100 !== 0) {
+    
+                result += ' ' + numberToText(number % 100);
+            }
+        }
+        result = `Минус ` + result;
+    }
+
+    else if(number < 20) {
+
+        result = decimals[number];
+    
+    } else if(number < 100) {
+    
+        result = tens[Math.floor(number / 10)];
+    
+        if(number % 10 !== 0) {
+    
+            result += ' ' + decimals[number % 10];
+        }
+        } else if (number < 1000) {
+    
+        result = hundreds [Math.floor(number / 100)];
+    
+        if (number % 100 !== 0) {
+    
+            result += ' ' + numberToText(number % 100);
+        }
+    }
+    
+    
+    
+    
+
+    
+    return result
+}
+
+let answerNumberToText
+
+
+
+
+// if(minValue <= -1000) {
+//     minValue = -999;
+// }
+
+
+
+// if(maxValue >= 1000) {
+//     maxValue = 999;-100000
+// }
+
+
+let answerNumber = Math.floor((minValue + maxValue) / 2);
 let orderNumber = 1;
 let gameRun = true;
+
 
 const orderNumberField = document.getElementById('orderNumberField');
 const answerField = document.getElementById('answerField');
 
 orderNumberField.innerText = orderNumber;
-answerField.innerText = `Вы загадали число ${answerNumber }?`;
+answerNumberToText = numberToText(answerNumber);
+answerField.innerText = `Вы загадали число ${answerNumberToText }?`;
 
 document.getElementById('btnRetry').addEventListener('click', function () {
-    minValue = 0;
-    maxValue = 100;
-    orderNumber = 0;
+    gameRun = true;
+    // minValue.number = document.querySelector('#inputMin').value
+    // maxValue = document.querySelector('#inputMax').value
+    
+    minValue = parseInt(prompt('Минимальное знание числа для новой игры','0'));
+    maxValue = parseInt(prompt('Максимальное знание числа для новой игры','100'));
+    if(isNaN(minValue)) minValue = 0 ;
+    if(isNaN(maxValue)) maxValue = 100 ;   
+    answerNumber  = Math.floor((minValue + maxValue) / 2);
+    orderNumber = 1;
+    orderNumberField.innerText = orderNumber;
+    answerNumberToText = numberToText(answerNumber);
+    answerField.innerText = `Вы загадали число ${answerNumberToText }?`;
+
+    
 })
+
+
+document.getElementById('btnLess').addEventListener('click', function () {
+    if (gameRun){
+        if (answerNumber <= minValue){
+            const phraseRandom = Math.round( Math.random() * 3);
+
+            const answerPhrase = (phraseRandom === 1) ?
+            `Вы загадали неправильное число!\n\u{1F914}` :
+            (phraseRandom === 2) ? `Я сдаюсь..\n\u{1F92F}` :
+            (phraseRandom === 3) ? `Что-то тут не так..\n\u{1F92F}` :
+            `А вы точно не врёте?...\n\u{1F92F}` ;
+
+            answerField.innerText = answerPhrase;
+
+            gameRun = false;
+            
+        } else {
+            maxValue = answerNumber  - 1;
+            answerNumber  = Math.floor((minValue + maxValue) / 2);
+            orderNumber++;
+            orderNumberField.innerText = orderNumber;
+            answerNumberToText = numberToText(answerNumber);
+            // Здесь продолжить...
+            answerField.innerText = `Вы загадали число ${answerNumberToText }?`;
+        }
+    }
+})
+
 
 document.getElementById('btnOver').addEventListener('click', function () {
     if (gameRun){
         if (minValue === maxValue){
-            const phraseRandom = Math.round( Math.random());
+            const phraseRandom = Math.round( Math.random() * 3);
             const answerPhrase = (phraseRandom === 1) ?
-                `Вы загадали неправильное число!\n\u{1F914}` :
-                `Я сдаюсь..\n\u{1F92F}`;
+            `Вы загадали неправильное число!\n\u{1F914}` :
+            (phraseRandom === 2) ? `Я сдаюсь..\n\u{1F92F}` :
+            (phraseRandom === 3) ? `Что-то тут не так..\n\u{1F92F}` :
+            `А вы точно не врёте?...\n\u{1F92F}` ;
 
             answerField.innerText = answerPhrase;
             gameRun = false;
@@ -32,14 +183,22 @@ document.getElementById('btnOver').addEventListener('click', function () {
             answerNumber  = Math.floor((minValue + maxValue) / 2);
             orderNumber++;
             orderNumberField.innerText = orderNumber;
-            answerField.innerText = `Вы загадали число ${answerNumber }?`;
+            answerNumberToText = numberToText(answerNumber);
+            answerField.innerText = `Вы загадали число ${answerNumberToText }?`;
         }
     }
 })
 
 document.getElementById('btnEqual').addEventListener('click', function () {
     if (gameRun){
-        answerField.innerText = `Я всегда угадываю\n\u{1F60E}`
+        const answerRandom = Math.round( Math.random() * 3);
+        const randomEnding = (answerRandom === 1) ?
+        `Я всегда угадываю\n\u{1F60E}` :
+        (answerRandom === 2) ? `Я так и знал!\n\u{1F929}` :
+        (answerRandom === 3) ? `Это было просто!\n\u{1F601}` :
+        `Легче простого!\n\u{1F61B}` ;
+        answerField.innerText = randomEnding
+
         gameRun = false;
     }
 })
